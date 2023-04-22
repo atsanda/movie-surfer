fc3f94f5d931534862e8e804724451e78d82410b
Islam Iangurazov <islamyang123@gmail.com>
2023-04-22 20:14:10 +0300

    Implement logic to download the data

--- moviesurfer/cli/main.py
+++ moviesurfer/cli/main.py
@@ -1,2 +1,19 @@
+import os
+import wget
+import zipfile
+
+# Function to download data
+def download_data():
+    output_dir = './moviesurfer/data'
+    url = 'http://files.grouplens.org/datasets/movielens/ml-25m.zip'
+    filename = wget.download(url, out=output_dir)
+    
+    # extract data using zipfile
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+        
+    # delete zip file
+    os.remove(filename)
+    
 def main():
     print("Hello from CLI, functionality is to be added")
