9ee39bea29cc53eb8f9047cc8d50aec1bc040920
Islam Iangurazov <79479451+Yanguraz@users.noreply.github.com>
2023-04-27 10:09:36 +0300

    Implement logic to download the data (#11)
    * Implement logic to download the data
    
    * Correcting logic flaws
    
    * Omiting function call
    
    * Trying to fight Black
    
    * Fighting Black 2nd round
    
    * Black relinquished but Isort persists
    
    * Moving function into Data subdirectory
    
    * Logging fix and Keyboard interrupt extension
    
    * Temp file handling
    
    * Logger go to Global

---
+++ moviesurfer/data/download_data.py
@@ -0,0 +1,47 @@
+import logging
+import os
+import zipfile
+from tempfile import TemporaryDirectory
+
+import wget
+
+logger = logging.getLogger(__name__)
+
+
+# Function to download data
+def download_data(
+    output_dir="moviesurfer/data",
+    url="http://files.grouplens.org/datasets/movielens/ml-25m.zip",
+):
+    """
+    Downloads the MovieLens 25M Dataset, extracts it, and deletes the zip file.
+
+    :param output_dir: str, the output directory to save the downloaded data
+    :param url: str, the url address to download from
+    :return: None
+    """
+
+    # Check if movies.csv file exists in output_dir
+    movies_csv_file = os.path.join(output_dir, "ml-25m/movies.csv")
+
+    if os.path.exists(movies_csv_file):
+        raise Exception("Data already exists in the output directory.")
+
+    # Download Data while adressing temp files that are created if WGET is interrupted
+    try:
+        with TemporaryDirectory() as temp_dir:
+            temp_file = os.path.join(temp_dir, "temp_folder")
+            logger.warning("Downloading the MovieLens 25M Dataset...")
+            wget.download(url, temp_file)
+
+            with zipfile.ZipFile(temp_file, "r") as data_ref:
+                data_ref.extractall(output_dir)
+
+            os.remove(temp_file)
+
+        logger.warning("Data downloaded successfully.")
+
+    except KeyboardInterrupt:
+        logging.warning("\nThe download was interrupted.")
+        if os.path.exists(temp_file):
+            os.remove(temp_file)
