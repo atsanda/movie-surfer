76cfc7d44aeec2e9c86ef26e9b4d693f6642617a
Anna Kiseleva <kiseleva.a.e@gmail.com>
2023-04-29 17:37:08 +0300

    - Moved CLI to separate file - Fixed dependencies (?)

--- moviesurfer/cli/main.py
+++ moviesurfer/cli/main.py
@@ -1,2 +1,14 @@
-def main():
-    print("Hello from CLI, functionality is to be added")
+import click
+
+from moviesurfer.data.make_dataset import make_dataset
+
+@click.command()
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
+def main(input_filepath, output_filepath):
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
+    """
+    make_dataset(input_filepath, output_filepath)
+
+main()
--- moviesurfer/data/make_dataset.py
+++ moviesurfer/data/make_dataset.py
@@ -6,13 +6,7 @@ import click
 from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument("input_filepath", type=click.Path(exists=True))
-@click.argument("output_filepath", type=click.Path())
-def main(input_filepath, output_filepath):
-    """Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
-    """
+def make_dataset(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
 
