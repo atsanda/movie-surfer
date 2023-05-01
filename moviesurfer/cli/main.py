5548c57d4b03d669d82e3c4d62e155b79ffb5f3d
Anna Kiseleva <kiseleva.a.e@gmail.com>
2023-05-01 18:38:12 +0300

    - Black linter fixes applied

--- moviesurfer/cli/main.py
+++ moviesurfer/cli/main.py
@@ -2,6 +2,7 @@ import click
 
 from moviesurfer.data.make_dataset import make_dataset
 
+
 @click.command()
 @click.argument("input_filepath", type=click.Path(exists=True))
 @click.argument("output_filepath", type=click.Path())
@@ -10,5 +11,6 @@ def main(input_filepath, output_filepath):
     cleaned data ready to be analyzed (saved in ../processed).
     """
     make_dataset(input_filepath, output_filepath)
+
 
 main()
