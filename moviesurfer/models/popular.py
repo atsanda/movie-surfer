0c0b6177bbfdd2437b8fe75939f7b70aec6406ee
Artyom Tsanda <tsanda.artyom@gmail.com>
2023-05-06 19:05:47 +0200

    implemented recsys with most popular movies

--- moviesurfer/models/popular.py
+++ moviesurfer/models/popular.py
@@ -1,22 +1,49 @@
-from itertools import cycle, takewhile
+import logging
+from itertools import cycle
+from typing import Iterable, Optional
 
 import pandas as pd
 
+logger = logging.getLogger(__name__)
+
 
 class MostPopular:
+    """
+    A baseline recommendation model based on the most
+    popular item in the train set.
+    """
+
     def __init__(
         self,
         top_k: int = 100,
         movie_column: str = "movieId",
         user_column: str = "userId",
-        rating_column: str = "rating",
     ):
+        """
+        Initializes an instance of the MostPopular class.
+
+        Args:
+            top_k (int): The number of top items to consider for recommendation.
+                         Defaults to 100.
+            movie_column (str): The name of the column containing
+                                movie IDs. Defaults to "movieId".
+            user_column (str): The name of the column containing
+            user IDs. Defaults to "userId".
+        """
         self.top_k = top_k
         self.user_column = user_column
-        self.rating_column = rating_column
         self.movie_column = movie_column
 
     def fit(self, data: pd.DataFrame):
+        """
+        Fits the recommendation model to the provided data.
+        This method computes the most popular movies based
+        on the frequency of occurrence in the data.
+        It keeps only self.top_k most popular movies.
+
+        Args:
+            data (pd.DataFrame): The input DataFrame containing user-movie interactions.
+        """
         self.popularity = (
             data.groupby(self.movie_column)[self.user_column]
             .count()
@@ -24,13 +51,33 @@ class MostPopular:
             .head(self.top_k)
             .index
         )
+        logger.debug(f"{MostPopular} is successfully fit.")
 
-    def predict(self, references: list[int], n: int):
-        references = set(references)
+    def predict(self, n: int, references: Optional[Iterable[int]] = None):
+        """
+        Generates recommendations based on the most popular movies.
+        The method excludes already viewed movies mentioned in the references.
+
+        Args:
+            references (list[int]): A list of already chosen item IDs.
+            n (int): The number of recommendations to generate.
+
+        Returns:
+            list[int]: A list of item IDs representing the recommendations.
+        """
+        references = set(references or [])
         recs = []
-        for movie in takewhile(lambda x: len(recs) < n, cycle(self.popularity)):
-            if movie in references:
+        for i, movie in enumerate(cycle(self.popularity)):
+            is_first_cycle = i < len(self.popularity)
+
+            # otherwise it leads to an infinite loop
+            if is_first_cycle and movie in references:
                 continue
+
             recs.append(int(movie))
+
+            # exit condition
+            if len(recs) == n:
+                break
 
         return recs
