45469c67b4aad269887c3c5c4373dd49a811fdc7
Anna Kiseleva <kiseleva.a.e@gmail.com>
2023-05-13 13:04:09 +0300

    Linter fixes applied

--- moviesurfer/cli/main.py
+++ moviesurfer/cli/main.py
@@ -1,26 +1,28 @@
-import click
 import logging
 
+import click
+
 from moviesurfer.data.download_data import download_data
+
 logger = logging.getLogger(__name__)
+
 
 @click.group(chain=True)
 def cli():
     logger.info("moviesurfer cli started")
 
+
 @cli.command()
-@click.argument(
-    'download'
-)
+@click.argument("download")
 def data(download):
     """Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-        """
+    cleaned data ready to be analyzed (saved in ../processed).
+    """
     try:
         download_data()
     except FileExistsError:
         logger.error("No need to call 'download data', file exists")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
