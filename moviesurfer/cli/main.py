642ca2b1455cdc9c2bab9d3bc38e66e4dccd95a9
Anna Kiseleva <kiseleva.a.e@gmail.com>
2023-05-13 21:46:36 +0300

    replaced arguments with subcommands

--- moviesurfer/cli/main.py
+++ moviesurfer/cli/main.py
@@ -7,22 +7,24 @@ from moviesurfer.data.download_data import download_data
 logger = logging.getLogger(__name__)
 
 
-@click.group(chain=True)
+@click.group(chain = True)
 def cli():
     logger.info("moviesurfer cli started")
 
+@cli.command
+def data():
+    pass
 
-@cli.command()
-@click.argument("download")
-def data(download):
+
+@cli.command
+def download():
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
     try:
         download_data()
     except FileExistsError:
-        logger.error("No need to call 'download data', file exists")
-
+        click.echo("No need to call 'download data', file exists")
 
 if __name__ == "__main__":
     cli()
